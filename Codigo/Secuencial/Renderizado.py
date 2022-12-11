c6ef90343612d8e799e4538c5d60d947ca9136bb
alextprog <alexander.t1780@gmail.com>
2022-12-11 17:55:25 -0500

    metodo de entrada

---
+++ Codigo/Secuencial/Renderizado.py
@@ -0,0 +1,15 @@
+def entrada():    
+    n = int(input())
+    circulos = list()
+
+    for i in range(0,n):
+        x = input().split()
+        x = map(int,x)    
+        circulos.append(list(x))   
+    
+    print(circulos)             
+    return circulos
+
+if __name__ == "__main__":    
+    entrada()
+
