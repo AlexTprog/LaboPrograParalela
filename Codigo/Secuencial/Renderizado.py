d5a548fb309bb3445ddebcb333c399d19061ce2a
alextprog <alexander.t1780@gmail.com>
2022-12-11 19:17:14 -0500

    fondo y rectangulo colores

--- Codigo/Secuencial/Renderizado.py
+++ Codigo/Secuencial/Renderizado.py
@@ -1,57 +1,75 @@
 import array
 
-HEIGHT = 1024
-WIDTH = 960
+WIDTH = 1024
+HEIGHT = 960
+
 
 def create_image():
-    data = []
+    red = []
+    green = []
+    blue = []
     for i in range(HEIGHT*WIDTH):
-        data.append([49, 196, 157])    
-    writePPM(data, WIDTH, HEIGHT, "output")
+        red.append(49)  
+        green.append(196)  
+        blue.append(157)  
+    writePPM(red, green, blue, "output.ppm")
 
-def writePPM(data, width, height, filename):    
-    ppm_header = f'P6 {width} {height} {255}\n'
-    rgb = []
-    for f in data:
-        rgb.append(f[0])  # Red
-        rgb.append(f[1])  # Green
-        rgb.append(f[2])  # Blue
-    
-    image = array.array('B', rgb)        
+def writePPM(red, green, blue, filename):
+  ppm_header = f'P6 {WIDTH} {HEIGHT} {255}\n'
+  rgb = []
+  for i in range(len(red)):
+      rgb.append(red[i]) # Red 
+      rgb.append(blue[i]) # Blue
+      rgb.append(green[i]) # Green 
+  image = array.array('B', rgb)
+  with open(filename, 'wb') as f:
+    f.write(bytearray(ppm_header, 'ascii'))
+    image.tofile(f)
 
-    with open(filename + '.ppm', 'wb') as f:        
-        f.write(bytearray(ppm_header, 'ascii'))
-        image.tofile(f)
+
 
 def readPPM(path):
-    data = []
-
-    with open(path,"rb") as f:
+    red = []
+    green = []
+    blue = []
+    width = 0
+    height = 0
+    with open(path, "rb") as f:
         # read header
         header = f.read(2)
-        f.read(1)  # blank
+        f.read(1) # blank
         width = f.read(3)
-        f.read(1)  # blank
+        f.read(1) # blank
         height = f.read(3)
-        f.read(1)  # blank
-        max_val = f.read(3)  # blank
+        f.read(1) # blank
+        max_val = f.read(3) # blank
         while (byte := f.read(1)):
-            value = int.from_bytes(byte, byteorder='big')
-            data.append(value)            
-            f.read(1)  # G
-            f.read(1)  # B
+            r = int.from_bytes(byte, byteorder='big')
+            red.append(r) # R
+            g = int.from_bytes(f.read(1), byteorder='big')
+            green.append(g) # G
+            b = int.from_bytes(f.read(1), byteorder='big')
+            blue.append(b) # B
 
-    return data
+    return red, green, blue
 
 def draw_rectangle():
-    data = readPPM("output.ppm")
-    width = 640
-    height = 480
-    from_point = width*10
-    to_point = width*100
-    for i in range(from_point, to_point):        
-        data[i] = 255  # negro
-    writePPM(data, width, height, "output")
+    red, green, blue = readPPM("output.ppm")
+    from_point = WIDTH*10
+    to_point = WIDTH*100
+
+    for i in range(from_point, to_point):                           
+        red[i]= red[i] ^ 138
+        green[i]= green[i] ^ 29
+        blue[i]= blue[i] ^ 171
+    writePPM(red,green,blue,"output.ppm")
+
+def combinarColor(data,new_data):
+    color = []
+    for i in range(WIDTH*HEIGHT):
+        new_color= data[i] ^ new_data[i]
+        color.append(int(new_color))
+    return color
 
 def entrada():
     n = int(input())
@@ -65,6 +83,5 @@ def entrada():
 
 if __name__ == "__main__":    
     circulos = entrada()
-    print(circulos)
     create_image()
-
+    draw_rectangle()
